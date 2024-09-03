d81d72231166013c527fbe74b7fe7757e2eeba19
ac3labador <ac3labador@gmail.com>
2024-09-03 03:17:01 +0000

    removed navbar for now

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,9 @@
-// pages/index.js
 import React, { useState } from 'react';
 import ListSection from '../components/ListSection';
-import SidePanel from '../components/SidePanel';
-import Navbar from '../components/Navbar';
 import WelcomePage from '../components/WelcomePage'; // Import the WelcomePage component
 
 export default function Home() {
   const [isWelcomePageVisible, setIsWelcomePageVisible] = useState(true); // State to manage the visibility of the welcome page
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
 
   const handleStart = () => {
     setIsWelcomePageVisible(false); // Hide the welcome page when the button is clicked
@@ -25,14 +17,8 @@ export default function Home() {
       ) : (
         // Render the main content
         <div className="flex h-screen">
-          {/* Sidebar for mobile and desktop */}
-          <SidePanel isOpen={isSidebarOpen} onClose={toggleSidebar} />
-
           {/* Main content area */}
-          <div className={`flex flex-col w-full transition-all duration-300 ease-in-out ${isSidebarOpen ? 'ml-64' : 'ml-0'}`}>
-            {/* Conditionally render Navbar */}
-            <Navbar toggleSidebar={toggleSidebar} />
-
+          <div className="flex flex-col w-full">
             {/* Main content */}
             <div className="flex-1 p-4 bg-gradient-to-r from-purple-500 to-blue-500">
               <h1 className="text-4xl pt-20 text-white font-bold mb-6 text-center">Welcome to DevHub</h1>
